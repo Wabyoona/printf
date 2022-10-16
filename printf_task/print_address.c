f9191969c7c41940627bc0f46834bd32e150c7e5
Gedralge <Gedralgerrol@gmail.com>
2022-10-16 07:48:09 -0700

    print_address.c

--- printf_task/print_address.c
+++ printf_task/print_address.c
@@ -10,14 +10,15 @@
 int print_address(va_list l, flags_t *f)
 {
 	char *str;
-	unsigned long int m va_arg(l, unsigned long int);
+	unsigned long int p = va_arg(l, unsigned long int);
 
 	register int count = 0;
 
 	(void)f;
 
-	if (!m)
-		return (_puts("(nil)"));                                                                                          str = convert(m, 16, 1);
+	if (!p)
+		return (_puts("(nil)"));
+	str = convert(p, 16, 1);
 	count += _puts("0x");
 	count += _puts(str);
 	return (count);
