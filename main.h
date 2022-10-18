632406eb7b67d3dad4ef5abe241631a84f86fd1c
Gedralge <Gedralgerrol@gmail.com>
2022-10-18 12:39:11 -0700

    main.h

--- main.h
+++ main.h
@@ -5,9 +5,11 @@
 #include <stdarg.h>
 
 /**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus:  flag for the '+' charact6er
+ * struct flags - struct containing
+ * flags to "turn on"
+ * when a flag specifier is passed
+ * to _printf()
+ * @plus: flag for the '+' character
  * @space: flag for the ' ' character
  * @hash: flag for the '#' character
  */
@@ -19,8 +21,10 @@ typedef struct flags
 } flags_t;
 
 /**
- * struct printHandler - struct to choose the right function dep[ending
- * on the foramat specifier passed to _printf()
+ * struct printHandler - struct to 
+ * choose the right function depending
+ * on the format specifier passed
+ * to _printf()
  * @c: format specifier
  * @f: pointer to the correct printing function
  */
@@ -38,7 +42,7 @@ int count_digit(int i);
 
 /* print_bases */
 int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l,  flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
 int print_octal(va_list l, flags_t *f);
 
@@ -56,7 +60,6 @@ int get_flag(char s, flags_t *f);
 
 /* print_alpha */
 int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
 
 /* write_funcs */
 int _putchar(char c);
@@ -67,10 +70,10 @@ int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
 
-/* print_adress */
-int print_adress(va_list l, flags_t *f);
+/* print_address */
+int print_address(va_list l, flags_t *f);
 
-/* print_percent */
+/* print_percen */
 int print_percent(va_list l, flags_t *f);
 
 
